a1b0c4b19228b05a38e606e0bcb8253df954dbe9
Nikhil Goswami <nikhil.goswami.18121999@gmail.com>
2025-01-09 18:55:51 +0530

    updating image generation project

--- AI-Image-Generation-App/src/App.jsx
+++ AI-Image-Generation-App/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+let [text, setText] = useState("");
+let [imgLink, setImgLink] = useState("");
+let [isGenerated, setIsGenerated] = useState(null);
+
+
+async function fetchData() {
+  const options = {
+    method: 'POST',
+    url: 'https://chatgpt-42.p.rapidapi.com/texttoimage3',
+    headers: {
+      'x-rapidapi-key': '41ef75cbcamsh58da2adee279124p18cba7jsn021699ab6e69',
+      'x-rapidapi-host': 'chatgpt-42.p.rapidapi.com',
+      'Content-Type': 'application/json'
+    },
+    data: {
+      text: `${text}`,
+      width: 512,
+      height: 512
+    }
+  };
+  
+  try {
+    const response = await axios.request(options);
+    setImgLink(response.data.generated_image);
+    
+  } catch (error) {
+    console.error(error);
+  } finally{
+    setIsGenerated(false);
+  }
+}
+
+const generateImage = () =>
+{
+  fetchData();
+  setIsGenerated(true);
+}
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>Image Generation App</h2>
+      <div className="input-container">
+        <input id='search-text' onChange={(e) =>{
+          setText(e.target.value)
+        }} className="text-input" type="text" />
+        <button onClick={generateImage} className="submit-btn">Submit</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="img-container">
+        {
+          isGenerated ? <h3>Image is being generated...</h3> : <img src={imgLink} alt="" />
+        }
+        
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
 export default App
